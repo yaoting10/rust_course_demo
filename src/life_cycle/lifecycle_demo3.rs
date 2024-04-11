28de4166a0229fdcc1b4c77349575de9836ddd2e
nick <coder@gmail.com>
2024-04-11 14:15:53 +0800

    lifecycle module add

--- src/life_cycle/lifecycle_demo3.rs
+++ src/life_cycle/lifecycle_demo3.rs
@@ -1,6 +1,6 @@
 fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
+    for (_, &item) in bytes.iter().enumerate() {
         if item == b' ' {
             return &s[0..1];
         }
